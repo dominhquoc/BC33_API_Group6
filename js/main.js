73a0f95acc0ca77508f5c68ec47ce4882b48ec9b
minhquoc0605 <minhquoc0605@gmail.com>
2022-08-25 01:51:38 +0700

    add

--- js/main.js
+++ js/main.js
@@ -19,14 +19,14 @@ function renderShoes(arr) {
   for (var i = 0; i < arr.length; i++) {
     var shoes = arr[i];
     html += `
-        <div class="card p-0">
+        <div class="card d-flex flex-column p-0">
                 <img src="${shoes.image}" alt="card-1" class="w-100">
                 <div class="card-body">
                     <h5 class="card-title">${shoes.name}</h5>
                     <p class="card-text">${shoes.description}</p>
                     <div class="d-flex justify-content-between align-item-center">                   
                         <button class="button-product col-6" onclick="buy('${shoes.id}')">
-                                <a href="../detail.html?productid=${shoes.id}" class="text-decoration-none fs-5 text-white">
+                                <a href="../detail.html?productid=${shoes.id}" class="text-decoration-none fs-5 text-dark">
                                      Buy now
                                 </a>
                         </button>                     
